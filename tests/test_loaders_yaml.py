d9e5b7fd4cd5363223f87b1e90479871014ab751
Michal Tarkowski <michal.tarkowski@addepto.com>
2024-05-16 08:58:49 +0200

    test for loading default message

--- tests/test_loaders_yaml.py
+++ tests/test_loaders_yaml.py
@@ -23,4 +23,8 @@ def test_load_variables():
     assert ts.steps[1].message.chat_uuid == "123e4567-e89b-12d3-a456-426614174000"
     assert ts.steps[1].message.asr_build == "{}"
 
+    assert ts.steps[2].message.message == "[reset]"
     assert ts.steps[2].message.sender_name == "John Doe"
+
+    assert ts.steps[3].message.message == "[qa] Hello!"
+    assert ts.steps[3].message.sender_name == "John Doe"
