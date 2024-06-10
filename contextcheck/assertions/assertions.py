75018aa4b940a999eff216683d0a16b885011997
Michal Tarkowski <michal.tarkowski@addepto.com>
2024-06-10 11:25:44 +0200

    Handle wrong evals in assertion

--- contextcheck/assertions/assertions.py
+++ contextcheck/assertions/assertions.py
@@ -22,5 +22,11 @@ class AssertionEval(AssertionBase):
 
     def __call__(self, response: ResponseBase) -> bool:
         if self.result is None:
-            self.result = bool(eval(self.eval))  # Should be bool
+            try:
+                result = eval(self.eval)
+            except NameError:
+                raise NameError(f"Given eval `{self.eval}` uses non-existent name.")
+            if not isinstance(result, bool):
+                raise ValueError(f"Given eval `{self.eval}` does not evaluate to bool.")
+            self.result = result
         return self.result
