f42ff8cf51ba93c6a9ca45e2c6a3a049b717863e
Bart <bartlomiej.grasza@addepto.com>
2024-06-12 18:08:07 +0200

    Fix tests

--- tests/test_llm_eval.py
+++ tests/test_llm_eval.py
@@ -17,19 +17,19 @@ def test_evals():
     assert test_steps[1].asserts[0].result == True
 
     # Test QA reference evaluator (invalid answer)
-    assert test_steps[1].asserts[0].result == False
+    assert test_steps[2].asserts[0].result == False
 
     # Test Model grading QA evaluator
-    assert test_steps[2].asserts[0].result == True
-
-    # Test Model grading QA evaluator (invalid answer)
-    assert test_steps[2].asserts[0].result == False 
-
-    # Test Summarization evaluator
     assert test_steps[3].asserts[0].result == True
 
+    # Test Model grading QA evaluator (invalid answer)
+    assert test_steps[4].asserts[0].result == False 
+
+    # Test Summarization evaluator
+    assert test_steps[5].asserts[0].result == True
+
     # Test Human vs AI evaluator
-    assert test_steps[4].asserts[0].result == True
+    assert test_steps[6].asserts[0].result == True
 
     # Test Human vs AI evaluator (wrong reference)
-    assert test_steps[5].asserts[0].result == False
+    assert test_steps[7].asserts[0].result == False
