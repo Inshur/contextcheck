d186f437ee64c428b93d417e63d5538c174532a3
Bart <bartlomiej.grasza@addepto.com>
2024-06-13 09:03:23 +0200

    Merge branch 'dev' into CC-11-Add-LLM-based-metrics

--- contextcheck/endpoints/endpoint_tg_chatbot.py
+++ contextcheck/endpoints/endpoint_tg_chatbot.py
@@ -3,24 +3,6 @@ from contextcheck.endpoints.endpoint import EndpointBase
 
 
 class EndpointTGChatBot(EndpointBase):
-<<<<<<< HEAD
-    class RequestModel(EndpointBase.RequestModel):
-        asr_build: dict | None = None
-
-        @field_validator("asr_build", mode="before")
-        @classmethod
-        def asr_converstion(cls, obj: str | dict) -> dict:
-            """Get ASR as dict or string encoded as JSON"""
-            if isinstance(obj, str):
-                return json.loads(obj)
-            elif isinstance(obj, dict):
-                return obj
-            else:
-                raise ValueError(
-                    "ASR provided in a wrong format (should be str or dict)"
-                )
-=======
->>>>>>> dev
 
     def model_post_init(self, __context) -> None:
         self.connector = ConnectorHTTP(
